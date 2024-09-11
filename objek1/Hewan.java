95c1ddf3d81e4a2b3797f8e7aa09b59f9e2e4cd4
Nabil Alif M <nabilalifmalikulmulki@gmail.com>
2024-09-11 09:07:13 +0700

    Add files via upload

---
+++ objek1/Hewan.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.dicoding.javafundamental.objek;
+
+/**
+ *
+ * @author NABIL
+ */
+public class Hewan {
+    public void cetakNama(String nama) {
+       System.out.println("Nama hewan: " + nama);
+    }
+}
