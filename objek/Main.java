fecbea992c759c41d85a9fded1ca96968d4ab3f1
Nabil Alif M <nabilalifmalikulmulki@gmail.com>
2024-09-11 09:02:46 +0700

    Add files via upload

---
+++ objek/Main.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.dicoding.javafundamental.objek;
+
+/**
+ *
+ * @author NABIL
+ */
+public class Main {
+     public static void main(String[] args) {
+        Hewan Elang = new Hewan("Elang");
+        Hewan Kucing = new Hewan("Kucing");
+
+
+        Elang.beratHewan(5);
+        Elang.jumlahKakiHewan(2);
+        Elang.cetakHewan();
+
+
+        Kucing.beratHewan(2);
+        Kucing.jumlahKakiHewan(4);
+        Kucing.cetakHewan();
+    }
+}
