53b20e29b8f01e2c331816a82a300f44d4a9c461
Nabil Alif M <nabilalifmalikulmulki@gmail.com>
2024-09-11 09:05:33 +0700

    Add files via upload

--- objek/Main.java
+++ objek/Main.java
@@ -9,18 +9,10 @@ package com.dicoding.javafundamental.objek;
  * @author NABIL
  */
 public class Main {
-     public static void main(String[] args) {
-        Hewan Elang = new Hewan("Elang");
-        Hewan Kucing = new Hewan("Kucing");
-
-
-        Elang.beratHewan(5);
-        Elang.jumlahKakiHewan(2);
-        Elang.cetakHewan();
-
-
-        Kucing.beratHewan(2);
-        Kucing.jumlahKakiHewan(4);
-        Kucing.cetakHewan();
+    public static void main(String[] args) {
+       // menciptakan object dengan nama 'objekHewan'
+       Hewan objekHewan = new Hewan();
+       objekHewan.cetakNama("Elang");
     }
+    
 }
